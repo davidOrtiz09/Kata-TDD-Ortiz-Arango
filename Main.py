69d2d84a51cff3abc316cd4ad5ca9936bb9071cb
David Ortiz <davideduardo.ortiz@gmail.com>
2017-10-01 16:02:47 -0500

    Calcular para 2: (Verde)

--- Main.py
+++ Main.py
@@ -6,6 +6,8 @@ class Procesador:
     def calcular(param):
         if param=="":
             return 0
+        elif len(param.split(",")) == 2:
+            return 2
         else:
             return 1
 
