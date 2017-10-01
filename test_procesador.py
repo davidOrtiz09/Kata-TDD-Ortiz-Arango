e75e3d8fc8c51e18dc74bb0b995febc119821906
David Ortiz <davideduardo.ortiz@gmail.com>
2017-10-01 17:01:00 -0500

    Calcular minimo 2 numeros, (Rojo)

--- test_procesador.py
+++ test_procesador.py
@@ -19,4 +19,7 @@ class TestProcesador(TestCase):
             self.assertEqual(Procesador.calcular("")[1], 0, "No son iguales")
 
     def test_calcular_minimo_tamanho1(self):
-             self.assertEqual(Procesador.calcular("1")[1],1, "No son iguales")
+             self.assertEqual(Procesador.calcular("1")[1], 1, "No son iguales")
+
+    def test_calcular_minimo_tamanho2(self):
+             self.assertEqual(Procesador.calcular("2,1")[1], 1, "No son iguales")
