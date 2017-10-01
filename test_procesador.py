fb9dad366f8f06e9f119b5c69378c9f2e19ce4fd
David Ortiz <davideduardo.ortiz@gmail.com>
2017-10-01 15:51:08 -0500

    Calcular para 2 numeros (Rojo)

--- test_procesador.py
+++ test_procesador.py
@@ -5,6 +5,10 @@ from Main import Procesador
 class TestProcesador(TestCase):
     def test_calcular_cadenavacia(self):
         self.assertEqual(Procesador.calcular(""), 0, "No son iguales")
+
     def test_calcular_tamanho1(self):
         self.assertEqual(Procesador.calcular("1"), 1, "No son iguales")
+
+    def test_calcular_tamanho2(self):
+            self.assertEqual(Procesador.calcular("1,2"), 2, "No son iguales")
 
